5d2d6a859bebb207ae9239c72befab46feac47e6
Ryan Trainor <rptrainor@gmail.com>
2020-06-30 18:47:12 -0700

    context api now works as expected

--- context/userContext.js
+++ context/userContext.js
@@ -1,11 +1,12 @@
-import { useState, useEffect, createContext, useContext } from 'react'
-import firebase from '../firebase/clientApp'
+import { useState, useEffect, createContext, useContext } from "react";
+import firebase from "../firebase/clientApp";
 
-export const UserContext = createContext()
+export const UserContext = createContext();
 
 export default function UserContextComp({ children }) {
-  const [user, setUser] = useState(null)
-  const [loadingUser, setLoadingUser] = useState(true) // Helpful, to update the UI accordingly.
+  const [user, setUser] = useState(null);
+  const [isBlocked, setIsBlocked] = useState(false);
+  const [loadingUser, setLoadingUser] = useState(true); // Helpful, to update the UI accordingly.
 
   useEffect(() => {
     // Listen authenticated user
@@ -13,28 +14,38 @@ export default function UserContextComp({ children }) {
       try {
         if (user) {
           // User is signed in.
-          const { uid, displayName, email, photoURL } = user
+          const { uid, displayName, email, photoURL } = user;
           // You could also look for the user doc in your Firestore (if you have one):
           // const userDoc = await firebase.firestore().doc(`users/${uid}`).get()
-          setUser({ uid, displayName, email, photoURL })
-        } else setUser(null)
+          setUser({ uid, displayName, email, photoURL });
+          firebase
+            .firestore()
+            .collection("users")
+            .doc(uid)
+            .get()
+            .then((doc) => {
+              if (doc.data().flag.flagged === true) {
+                setIsBlocked(true);
+              }
+            });
+        } else setUser(null);
       } catch (error) {
         // Most probably a connection error. Handle appropriately.
       } finally {
-        setLoadingUser(false)
+        setLoadingUser(false);
       }
-    })
+    });
 
     // Unsubscribe auth listener on unmount
-    return () => unsubscriber()
-  }, [])
+    return () => unsubscriber();
+  }, []);
 
   return (
-    <UserContext.Provider value={{ user, setUser, loadingUser }}>
+    <UserContext.Provider value={{ user, setUser, loadingUser, isBlocked }}>
       {children}
     </UserContext.Provider>
-  )
+  );
 }
 
 // Custom hook that shorhands the context!
-export const useUser = () => useContext(UserContext)
+export const useUser = () => useContext(UserContext);
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
-import UserProvider from '../context/userContext'
+import UserProvider from "../context/userContext";
+import "../styles/globalStyles.css";
 
-//
 // Custom App to wrap it with context provider
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
       <Component {...pageProps} />
     </UserProvider>
-  )
+  );
 }
