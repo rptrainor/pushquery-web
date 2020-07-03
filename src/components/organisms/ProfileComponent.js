c397d04a97504c963ea1b87e57506a7abbc4f18f
Ryan Trainor <rptrainor@gmail.com>
2020-07-02 22:17:31 -0700

    rought draft of the app done

--- src/components/organisms/ProfileComponent.js
+++ src/components/organisms/ProfileComponent.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React from "react";
+import { useRouter } from "next/router";
+
+import firebase from "../../../firebase/clientApp";
+import { useUser } from "../../../context/userContext";
+import LoginStyles from "../../../styles/login.module.css";
 
 export default function ProfileComponent() {
+  const { loadingUser, user, isBlocked } = useUser();
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    router.push("/");
+    await firebase.auth().signOut();
+  };
+
   return (
-    <>
-    <h1>ProfileComponent.js</h1>
-    </>
+    <fieldset className={LoginStyles.fieldset}>
+      <div className={LoginStyles.container}>
+        <h1 className={LoginStyles.header}>
+          Hello {user.displayName}, would you like to sign out?
+        </h1>
+        <button className={LoginStyles.button} onClick={handleSignOut}>
+          <p className={LoginStyles.buttonText}>SIGN OUT</p>
+        </button>
+      </div>
+    </fieldset>
   );
 }
